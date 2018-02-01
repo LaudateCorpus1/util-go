c3f6e2d5f21a06161ffd4cbca2569df944ddd098
lsautia <lsautia@viantinc.com>
2018-01-31 19:10:43 -0700

    Adding json to java property marshaler

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # util-go
+
+TODO
+Need to add comments to exported functions
+Fix licensing on fields
--- encoding.go
+++ encoding.go
@@ -1,12 +1,16 @@
 package util
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"reflect"
+	"strconv"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
-func MarshalData(data map[string]interface{}, format string) []byte {
+func MarshalData(data interface{}, format string) []byte {
 	var marshaledData []byte
 
 	switch format {
@@ -16,20 +20,240 @@ func MarshalData(data map[string]interface{}, format string) []byte {
 		marshaledData, _ = json.MarshalIndent(data, "", "    ")
 	case "yaml":
 		marshaledData, _ = yaml.Marshal(data)
+	case "java":
+		marshaledData = JsonToJavaProperties(data)
 	}
 
 	return marshaledData
 }
 
-func UnmarshalData(byteData []byte, format string) map[string]interface{} {
-	var data map[string]interface{}
+func UnmarshalData(byteData []byte, format string) interface{} {
+	var data interface{}
 
 	switch format {
 	case "json":
-		json.Unmarshal(byteData, &data)
+		err := json.Unmarshal(byteData, &data)
+		if err != nil {
+			fmt.Println(err)
+		}
 	case "yaml":
-		yaml.Unmarshal(byteData, &data)
+		err := yaml.Unmarshal(byteData, &data)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
 	return data
+}
+
+func JsonToJavaProperties(data interface{}) []byte {
+	buffer := new(bytes.Buffer)
+	var right interface{}
+	for k, v := range data.(map[string]interface{}) {
+		if fmt.Sprint(reflect.TypeOf(v)) == "map[string]interface {}" {
+			right = MarshalData(v, "json")
+		} else {
+			right = v
+		}
+
+		buffer.Write([]byte(fmt.Sprintf("%s = %v\n", k, right)))
+	}
+	return buffer.Bytes()
+}
+
+func JsonToJson(byteData []byte) []byte {
+	ud := UnmarshalData(byteData, "json")
+	return MarshalData(ud, "json")
+}
+
+func JsonToYaml(byteData []byte) []byte {
+	ud := UnmarshalData(byteData, "json")
+	return MarshalData(ud, "yaml")
+}
+
+func YamlToJson(byteData []byte) []byte {
+	ud := UnmarshalData(byteData, "yaml")
+	ytc, _ := YamlToCompatJson(ud, nil)
+	return MarshalData(ytc, "json")
+}
+
+func YamlToCompatJson(yamlObj interface{}, jsonTarget *reflect.Value) (interface{}, error) {
+	var err error
+
+	// Resolve jsonTarget to a concrete value (i.e. not a pointer or an
+	// interface). We pass decodingNull as false because we're not actually
+	// decoding into the value, we're just checking if the ultimate target is a
+	// string.
+	if jsonTarget != nil {
+		ju, tu, pv := indirect(*jsonTarget, false)
+		// We have a JSON or Text Umarshaler at this level, so we can't be trying
+		// to decode into a string.
+		if ju != nil || tu != nil {
+			jsonTarget = nil
+		} else {
+			jsonTarget = &pv
+		}
+	}
+
+	// If yamlObj is a number or a boolean, check if jsonTarget is a string -
+	// if so, coerce.  Else return normal.
+	// If yamlObj is a map or array, find the field that each key is
+	// unmarshaling to, and when you recurse pass the reflect.Value for that
+	// field back into this function.
+	switch typedYAMLObj := yamlObj.(type) {
+	case map[interface{}]interface{}:
+		// JSON does not support arbitrary keys in a map, so we must convert
+		// these keys to strings.
+		//
+		// From my reading of go-yaml v2 (specifically the resolve function),
+		// keys can only have the types string, int, int64, float64, binary
+		// (unsupported), or null (unsupported).
+		strMap := make(map[string]interface{})
+		for k, v := range typedYAMLObj {
+			// Resolve the key to a string first.
+			var keyString string
+			switch typedKey := k.(type) {
+			case string:
+				keyString = typedKey
+			case int:
+				keyString = strconv.Itoa(typedKey)
+			case int64:
+				// go-yaml will only return an int64 as a key if the system
+				// architecture is 32-bit and the key's value is between 32-bit
+				// and 64-bit. Otherwise the key type will simply be int.
+				keyString = strconv.FormatInt(typedKey, 10)
+			case float64:
+				// Stolen from go-yaml to use the same conversion to string as
+				// the go-yaml library uses to convert float to string when
+				// Marshaling.
+				s := strconv.FormatFloat(typedKey, 'g', -1, 32)
+				switch s {
+				case "+Inf":
+					s = ".inf"
+				case "-Inf":
+					s = "-.inf"
+				case "NaN":
+					s = ".nan"
+				}
+				keyString = s
+			case bool:
+				if typedKey {
+					keyString = "true"
+				} else {
+					keyString = "false"
+				}
+			default:
+				return nil, fmt.Errorf("Unsupported map key of type: %s, key: %+#v, value: %+#v",
+					reflect.TypeOf(k), k, v)
+			}
+
+			// jsonTarget should be a struct or a map. If it's a struct, find
+			// the field it's going to map to and pass its reflect.Value. If
+			// it's a map, find the element type of the map and pass the
+			// reflect.Value created from that type. If it's neither, just pass
+			// nil - JSON conversion will error for us if it's a real issue.
+			if jsonTarget != nil {
+				t := *jsonTarget
+				if t.Kind() == reflect.Struct {
+					keyBytes := []byte(keyString)
+					// Find the field that the JSON library would use.
+					var f *field
+					fields := cachedTypeFields(t.Type())
+					for i := range fields {
+						ff := &fields[i]
+						if bytes.Equal(ff.nameBytes, keyBytes) {
+							f = ff
+							break
+						}
+						// Do case-insensitive comparison.
+						if f == nil && ff.equalFold(ff.nameBytes, keyBytes) {
+							f = ff
+						}
+					}
+					if f != nil {
+						// Find the reflect.Value of the most preferential
+						// struct field.
+						jtf := t.Field(f.index[0])
+						strMap[keyString], err = YamlToCompatJson(v, &jtf)
+						if err != nil {
+							return nil, err
+						}
+						continue
+					}
+				} else if t.Kind() == reflect.Map {
+					// Create a zero value of the map's element type to use as
+					// the JSON target.
+					jtv := reflect.Zero(t.Type().Elem())
+					strMap[keyString], err = YamlToCompatJson(v, &jtv)
+					if err != nil {
+						return nil, err
+					}
+					continue
+				}
+			}
+			strMap[keyString], err = YamlToCompatJson(v, nil)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return strMap, nil
+	case []interface{}:
+		// We need to recurse into arrays in case there are any
+		// map[interface{}]interface{}'s inside and to convert any
+		// numbers to strings.
+
+		// If jsonTarget is a slice (which it really should be), find the
+		// thing it's going to map to. If it's not a slice, just pass nil
+		// - JSON conversion will error for us if it's a real issue.
+		var jsonSliceElemValue *reflect.Value
+		if jsonTarget != nil {
+			t := *jsonTarget
+			if t.Kind() == reflect.Slice {
+				// By default slices point to nil, but we need a reflect.Value
+				// pointing to a value of the slice type, so we create one here.
+				ev := reflect.Indirect(reflect.New(t.Type().Elem()))
+				jsonSliceElemValue = &ev
+			}
+		}
+
+		// Make and use a new array.
+		arr := make([]interface{}, len(typedYAMLObj))
+		for i, v := range typedYAMLObj {
+			arr[i], err = YamlToCompatJson(v, jsonSliceElemValue)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return arr, nil
+	default:
+		// If the target type is a string and the YAML type is a number,
+		// convert the YAML type to a string.
+		if jsonTarget != nil && (*jsonTarget).Kind() == reflect.String {
+			// Based on my reading of go-yaml, it may return int, int64,
+			// float64, or uint64.
+			var s string
+			switch typedVal := typedYAMLObj.(type) {
+			case int:
+				s = strconv.FormatInt(int64(typedVal), 10)
+			case int64:
+				s = strconv.FormatInt(typedVal, 10)
+			case float64:
+				s = strconv.FormatFloat(typedVal, 'g', -1, 32)
+			case uint64:
+				s = strconv.FormatUint(typedVal, 10)
+			case bool:
+				if typedVal {
+					s = "true"
+				} else {
+					s = "false"
+				}
+			}
+			if len(s) > 0 {
+				yamlObj = interface{}(s)
+			}
+		}
+		return yamlObj, nil
+	}
+
+	return nil, nil
 }
